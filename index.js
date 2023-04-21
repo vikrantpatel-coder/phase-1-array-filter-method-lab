eb2a3d279cee7288ecdb0a95c08fab3d6e1c3a6a
vikrantpatel-coder <126751705+vikrantpatel-coder@users.noreply.github.com>
2023-04-21 11:35:19 -0400

    array_fil ter_lab_complete

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function findMatching(drivers,name){
+    return drivers.filter((driverName)=>{
+    return driverName.toUpperCase() === name.toUpperCase();
+    })
+};
+
+ 
+function fuzzyMatch(drivers,letters){
+    let lengthOfLetters = letters.length;
+    return drivers.filter((driverName)=>{
+    return driverName.slice(0,lengthOfLetters) === letters;
+})
+
+};
+
+
+function matchName(drivers,name){
+    return drivers.filter((driverName)=>{
+        return driverName.name.toUpperCase() === name.toUpperCase();
+        })
+
+}
+
+
